0be61df90005cf8e86bd21b5f5212e48dbe63a86
ndmchuong <minhchuong24052001@gmail.com>
2022-10-08 20:48:21 +0700

    hover slideshow img and section recommend

--- main.js
+++ main.js
@@ -26,6 +26,67 @@ const arrImgTextBanner = [
     }
 
 ];
+
+const arrImgProduct = [
+    {
+        index: 0,
+        src: ["./img/Vehicles/A-class/home-a35-1.jpg", "./img/Vehicles/A-class/home_mercedes-benz_amg-a-35_2.webp", "./img/Vehicles/A-class/home_mercedes-benz_amg-a-35_3.jpg", "./img/Vehicles/A-class/home_mercedes-benz_amg-a-35_4.jpg", "./img/Vehicles/A-class/home-mercedes-amg-a35-4matic-5.jpg"]
+    },
+    {
+        index: 1,
+        src: ["./img/Vehicles/C-class/home-Mercedes-Benz-C-Class-1.jpg","./img/Vehicles/C-class/home-mercedes-benz-c-class-2.jpg", "./img/Vehicles/C-class/home-mercedes-benz-c-class-3.jpg", "./img/Vehicles/C-class/home-mercedes-benz-c-class-4.jpg", "./img/Vehicles/C-class/home-Mercedes-Benz-C-Class-5.jpg"]
+    },
+    {
+        index: 2,
+        src: ["./img/Vehicles/E-class/home-Mercedes-e-class-1.jpg", "./img/Vehicles/E-class/home-mercedes-e300-amg-2.jpg", "./img/Vehicles/E-class/home-mercedes-benz-e-class-3.jpg", "./img/Vehicles/E-class/home-mercedes-benz-e-class-4.jpg", "./img/Vehicles/E-class/home-mercedes-benz-e-class-5.jpg"]
+    },
+    {
+        index: 3,
+        src: ["./img/Vehicles/S-class/home-Mercedes-Benz-S-Class-1.jpg", "./img/Vehicles/S-class/home-mercedes-benz-s-class-2.jpg", "./img/Vehicles/S-class/home-mercedes-benz-s-class-3.jpg", "./img/Vehicles/S-class/home-mercedes-benz-s-class-sedan-4.jpg", "./img/Vehicles/S-class/home-mercedes-benz-s-class-sedan-5.jpg"]
+    },
+    {
+        index: 4,
+        src: ["./img/Vehicles/S-class/home-maybach-1.jpg", "./img/Vehicles/S-class/home-mercedes-maybach-s-2.jpg", "./img/Vehicles/S-class/home-mercedes-maybach-s-3.jpg", "./img/Vehicles/S-class/home-mercedes-maybach-s-4.jpg", "./img/Vehicles/S-class/home-mercedes-maybach-s-5.jpg"]
+    },
+    {
+        index: 5,
+        src: ["./img/Vehicles/GLB/home-glb200amg-1.jpg", "./img/Vehicles/GLB/home-mercedes-amg-glb-35-2.jpg", "./img/Vehicles/GLB/home-mercedes-amg-glb-35-3.jpg", "./img/Vehicles/GLB/home-mercedes-amg-glb-35-4.jpg", "./img/Vehicles/GLB/home-mercedes-amg-glb-35-5.jpg"]
+    },
+    {
+        index: 6,
+        src: ["./img/Vehicles/GLC/home-mercedes-benz-glc-200-4matic-1.jpg", "./img/Vehicles/GLC/home-mercedes-benz-glc-2.jpg", "./img/Vehicles/GLC/home-mercedes-benz-glc-3.jpg", "./img/Vehicles/GLC/home-mercedes-benz-glc-4.jpg", "./img/Vehicles/GLC/home-mercedes-benz-glc-5.jpg"]
+    },
+    {
+        index: 7,
+        src: ["./img/Vehicles/GLC/home-GLC_300_4Matic_Coupe.jpg", "./img/Vehicles/GLC/home-mercedes-benz-glc-class-coupe-2.jpg", "./img/Vehicles/GLC/home-mercedes-benz-glc-class-coupe-3.jpg", "./img/Vehicles/GLC/home-mercedes-benz-glc-class-coupe-4.jpg", "./img/Vehicles/GLC/home-mercedes-benz-glc-class-coupe-5.jpg"]
+    },
+    {
+        index: 8,
+        src: ["./img/Vehicles/GLE/home-mercedes-amg-gle-suv-1.jpg", "./img/Vehicles/GLE/home-mercedes-benz-gle-2.jpg", "./img/Vehicles/GLE/home-mercedes-benz-gle-3.jpg", "./img/Vehicles/GLE/home-mercedes-benz-gle-4.jpg", "./img/Vehicles/GLE/home-mercedes-benz-gle-5.jpg"]
+    },
+    {
+        index: 9,
+        src: ["./img/Vehicles/GLE/home-mercedes-amg-gle-coupe-1.jpg", "./img/Vehicles/GLE/home-mercedes-amg-gle-coupe-2.jpg", "./img/Vehicles/GLE/home-mercedes-amg-gle-53-coupe-3.jpg", "./img/Vehicles/GLE/home_mercedes_gle_53_coupe_4.jpg", "./img/Vehicles/GLE/home_mercedes_gle_53_coupe_5.jpg"]
+    },
+    {
+        index: 10,
+        src: ["./img/Vehicles/GLS/home-mercedes-gls-1.jpg", "./img/Vehicles/GLS/home_mercedes_gls_2.jpg", "./img/Vehicles/GLS/home_mercedes_gls_3.jpg", "./img/Vehicles/GLS/home_mercedes_gls_4.jpg", "./img/Vehicles/GLS/home_mercedes_gls_5.jpg"]
+    },
+    {
+        index: 11,
+        src: ["./img/Vehicles/G-Class/home-mercedes-amg-g63-1.jpg", "./img/Vehicles/G-Class/home-mercedes-amg-g63-2.jpg", "./img/Vehicles/G-Class/home-mercedes-g63-amg-3.jpg", "./img/Vehicles/G-Class/home-mercedes-g63-amg-4.jpg", "./img/Vehicles/G-Class/home-mercedes-amg-g63-5.jpg"]
+    },
+    {
+        index: 12,
+        src: ["./img/Vehicles/GLS/home-maybach-gls-1.jpg", "./img/Vehicles/GLS/home-Mercedes-Maybach-GLS-2.jpg", "./img/Vehicles/GLS/home-Mercedes-Maybach-GLS-3.jpg", "./img/Vehicles/GLS/home-Mercedes-Maybach-gls-4.jpg", "./img/Vehicles/GLS/home-Mercedes-Maybach-GLS-5.jpg"]
+    }, 
+    {
+        index: 13,
+        src: ["./img/Vehicles/AMG-GT-4-door/home-mercedes-amg-gt-4-door-1.jpg", "./img/Vehicles/AMG-GT-4-door/home-mercedes-amg-gt-4-door-2.jpg", "./img/Vehicles/AMG-GT-4-door/home-mercedes-amg-gt-4-door-3.jpg", "./img/Vehicles/AMG-GT-4-door/home-mercedes-amg-gt-4-door-4.jpg", "./img/Vehicles/AMG-GT-4-door/home-mercedes-amg-gt-4-door-5.jpg"]
+    }
+]
+
+
 let indexSlide = 0;
 const handleSlideBanner = () => {
     indexSlide += 1;
@@ -42,6 +103,54 @@ const handleSlideBanner = () => {
     textModel.innerText = `${arrImgTextBanner[indexSlide].textModel}`;
     textSlogan.innerText = `${arrImgTextBanner[indexSlide].textSlogan}`;
     linkDetail.href = `${arrImgTextBanner[indexSlide].href}`;
-
 } 
 setInterval(handleSlideBanner, 5000);
+
+const handleOnOffNav = () => {
+    const navBarNav = document.querySelector(".navbar-nav");
+    const iconBar = document.querySelector(".btn-on-off-navigation").children[0];
+    
+    if (iconBar.className === 'fa fa-bars') {
+        iconBar.className = "fa fa-close";
+    }
+    else if (iconBar.className === "fa fa-close") {
+        iconBar.className = "fa fa-bars";
+    }
+    navBarNav.classList.toggle("dp-flex");
+}
+
+
+let indexSlideshowImg = 1;
+let showImg;
+const handleSlideshow = (index) => {
+    // Lay ra mang thong tin san pham trung voi san pham duoc chi dinh
+    let infoProduct = arrImgProduct.filter(value => value.index === index);
+    // Lay the img va mang cac src image
+    let productItemImg = document.querySelectorAll(".product-item-img")[infoProduct[0].index];
+    let srcImgProduct = infoProduct[0].src;
+    // set src cua the img
+    productItemImg.src = `${srcImgProduct[indexSlideshowImg]}`;
+    // set image tu chuyen doi sau t(s)
+    showImg = setInterval(() => {
+        indexSlideshowImg += 1;
+        if (indexSlideshowImg === srcImgProduct.length) {
+            indexSlideshowImg = 1;
+        };
+        productItemImg.src = `${srcImgProduct[indexSlideshowImg]}`;
+    }, 2000);
+}
+
+const handleStopSlideshow = (index) => {
+    clearInterval(showImg);
+    // Lay ra mang thong tin san pham trung voi san pham duoc chi dinh
+    let infoProduct = arrImgProduct.filter(value => value.index === index);
+    // Lay the img va mang cac src image
+    let productItemImg = document.querySelectorAll(".product-item-img")[infoProduct[0].index];
+    let srcImgProduct = infoProduct[0].src;
+    // set src cua the img
+    productItemImg.src = `${srcImgProduct[0]}`;
+    indexSlideshowImg = 1;
+}
+
+
+
